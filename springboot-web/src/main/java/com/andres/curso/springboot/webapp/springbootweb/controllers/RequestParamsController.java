618a830709baa962b78dfb91b18b1ecb7afb3e33
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2025-07-28 23:36:10 -0600

    first commit

---
+++ springboot-web/src/main/java/com/andres/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
@@ -0,0 +1,24 @@
+package com.andres.curso.springboot.webapp.springbootweb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andres.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+    
+
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam String 
+    
+    message){
+
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+}
